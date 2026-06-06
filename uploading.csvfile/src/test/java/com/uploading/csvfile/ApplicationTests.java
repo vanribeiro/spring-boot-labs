c14d7d7d877d3de153d7221662209f2e7a0707dd
Van Ribeiro <vanribeiro.dev@gmail.com>
2026-06-06 00:46:29 -0300

    initial files

---
+++ uploading.csvfile/src/test/java/com/uploading/csvfile/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploading.csvfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
